99bde8387fa021a2402c66852fd469a342681d94
antran2704 <phamtrangiaan27@gmail.com>
2022-11-14 20:31:18 +0700

    up code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./src/db/index");
 const routes = require("./src/routers/index")
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const cors = require('cors');
 
 db.connect();
--- src/db/index.js
+++ src/db/index.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose")
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/TodoApp');
+        await mongoose.connect('mongodb+srv://antran:cunyeu1506@cluster0.mflf25c.mongodb.net/?retryWrites=true&w=majority');
         console.log("connected database succesfully")
     } catch (error) {
         console.log(error, "connect database false")
+
     }
 }
 
